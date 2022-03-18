39728ff570cc97260a45527e1180dbeb0cd9d4a7
francoparnetti <francoparnetti@gmail.com>
2022-03-18 19:17:39 -0300

    Mas cambios sobre los templates para empezar a trabajar con las clases y los formularios

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,9 @@
 from django import urls
 
 from django.urls import path
-from .views import index
+from .views import index, forms
 
 urlpatterns = [
     path("", index, name = "index"),
+    path("forms/", forms, name = "forms")
 ]
--- index/views.py
+++ index/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index/homepage.html", {})
+
+
+def forms(request):
+    return render(request, "index/forms.html", {})
