b0335bd6c7e8137238ccdd7407de177ed9ea029d
Gabriel Graterol <ssbpf01@gmail.com>
2022-03-18 21:41:57 -0400

    Se registraron los models en admin

---
+++ class/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from Class.models import Students, Teachers, Careers
+
+# Register your models here.
+
+admin.site.register(Students)
+admin.site.register(Teachers)
+admin.site.register(Careers)
+
+
+
+
