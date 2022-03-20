e4fb80e9d409da1eeb4086b63b4f444420bc62db
Gabriel Graterol <ssbpf01@gmail.com>
2022-03-20 15:15:43 -0400

    Se elimino el email de la tabla Students y Teachers de models.py

--- class/models.py
+++ class/models.py
@@ -3,15 +3,18 @@ from django.db import models
 class Teachers(models.Model):
     name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField()
     subject_matter = models.CharField(max_length=30)
+
 
 class Students(models.Model):
     name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField()
     career = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
+        
 class Careers(models.Model):
     name = models.CharField(max_length=50)
-    serie = models.IntegerField(max_length=10)
+    serie = models.IntegerField()
+
