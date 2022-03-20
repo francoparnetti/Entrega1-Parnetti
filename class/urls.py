f8198b5ff57db19502d195d894e29ae04e9cd7b9
Gabriel Graterol <ssbpf01@gmail.com>
2022-03-20 15:18:58 -0400

    Se agregaron urls al Class/urls.py

--- class/urls.py
+++ class/urls.py
@@ -1,4 +1,10 @@
+from django.urls import path
+from .views import forms, students_list
 
+urlpatterns = [
+    path("forms/", forms, name="Forms"),
+    path("students",students_list, name="students_list")
+]
 
     
 
