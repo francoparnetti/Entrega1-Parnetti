09bcd40583a1ce4ebf317ee8da3ad7c0f9a0d738
francoparnetti <francoparnetti@gmail.com>
2022-04-15 15:58:10 -0300

    CRUD completo de los blogs (basicos)

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django import urls
 
 from django.urls import path
-from .views import create_blog, blog_feed
+from . import views
 
 urlpatterns = [
-    path("create/", create_blog, name="create_blog"),
-    path("feed/", blog_feed, name="blog_feed"),
+    path("create/", views.create_blog, name="create_blog"),
+    path("feed/", views.blog_feed, name="blog_feed"),
+    path("<int:pk>/", views.BlogDetail.as_view(), name="blog_detail"),
+    path("<int:pk>/edit/", views.BlogEdit.as_view(), name="blog_edit"),
+    path("<int:pk>/delete/", views.BlogDelete.as_view(), name="blog_delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from .forms import CreateBlog, BlogSearch
 from .models import Blog
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
-
+login_required
 def create_blog(request):
     if request.method == "POST":
         form = CreateBlog(request.POST)
@@ -27,3 +31,21 @@ def blog_feed(request):
         blogs = Blog.objects.all()
     form = BlogSearch()
     return render(request, "blog/blog_feed.html", {"form": form, "blogs": blogs}) 
+
+
+
+
+class BlogDetail(DetailView):
+    model = Blog
+    template_name = "blog/blog_detail.html"
+    
+class BlogEdit(LoginRequiredMixin, UpdateView):
+    model = Blog
+    template_name = "blog/blog_edit.html"
+    success_url = "/blog/feed/"
+    fields = ["title", "subtitle"]
+
+class BlogDelete(LoginRequiredMixin, DeleteView):
+    model = Blog
+    template_name = "blog/blog_delete.html"
+    success_url = "/blog/feed/"
